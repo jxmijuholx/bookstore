490b68983576fd36f15e47c5da1ee30111d433a4
Jami Juhola <jami.juhola01@gmail.com>
2023-09-15 19:35:06 +0300

    tehtävä 3.3

--- src/main/java/hh/soft03/bookstore/web/BookstoreController.java
+++ src/main/java/hh/soft03/bookstore/web/BookstoreController.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import hh.soft03.bookstore.domain.Book;
 import hh.soft03.bookstore.domain.BookRepository;
@@ -33,5 +36,26 @@ public class BookstoreController {
 		
 		return "booklist";
 	}
+	
+	   @GetMapping("/addbook")
+	    public String showAddBookForm(Model model) {
+	        model.addAttribute("newBook", new Book());
+	        return "addbook";
+	    }
+
+	   
+	    @PostMapping("/addbook")
+	    public String addBook(@ModelAttribute("newBook") Book newBook) {
+	       
+	        bookRepository.save(newBook);
+	        return "redirect:/booklist"; 
+	    }
+
+	    // Delete a book by ID
+	    @GetMapping("/delete/{id}")
+	    public String deleteBook(@PathVariable Long id) {
+	        bookRepository.deleteById(id);
+	        return "redirect:/booklist";
+	    }
 	
 }
