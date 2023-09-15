f88a6b12a966d0a6bc575b63d42eae5052d358e9
Jami Juhola <jami.juhola01@gmail.com>
2023-09-15 19:15:45 +0300

    tehtävä 3.2

--- src/main/java/hh/soft03/bookstore/BookstoreApplication.java
+++ src/main/java/hh/soft03/bookstore/BookstoreApplication.java
@@ -19,8 +19,8 @@ public class BookstoreApplication {
 	@Bean
 	public CommandLineRunner demo(BookRepository repository) {
 	return (args) -> {
-		 Book book1 = new Book("Sample Book 1", "Author 1", 2023, "ISBN123", 29.99);
-	     Book book2 = new Book("Sample Book 2", "Author 2", 2023, "ISBN456", 19.99);
+		 Book book1 = new Book("Jukka", "Juustola", 2023, "ISBN123", 29.99);
+	     Book book2 = new Book("Jassi", "Jaatila", 2023, "ISBN456", 19.99);
 	        
 	     repository.save(book1);
 	     repository.save(book2);
--- src/main/java/hh/soft03/bookstore/web/BookstoreController.java
+++ src/main/java/hh/soft03/bookstore/web/BookstoreController.java
@@ -1,8 +1,14 @@
 package hh.soft03.bookstore.web;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import hh.soft03.bookstore.domain.Book;
+import hh.soft03.bookstore.domain.BookRepository;
 
 
 
@@ -10,10 +16,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 public class BookstoreController {
 	
+	@Autowired
+	BookRepository bookRepository;
+	
 	@GetMapping("/index")
 	public String index(Model model) {
 		return "index";
 		
+	}
+	
+	@GetMapping("/booklist")
+	public String booklist(Model model) {
+		List<Book> books =  (List<Book>) bookRepository.findAll();
+	     
+		model.addAttribute("books", books);
+		
+		return "booklist";
 	}
 	
 }
