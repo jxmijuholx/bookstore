77c38e546d6200130fe380f612160025c6b3db48
Jami Juhola <jami.juhola01@gmail.com>
2023-09-28 13:01:15 +0300

    auto-generated rest

--- src/main/java/hh/soft03/bookstore/web/BookstoreController.java
+++ src/main/java/hh/soft03/bookstore/web/BookstoreController.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -100,5 +102,20 @@ public class BookstoreController {
     	}
     	
     }
+    
+    //this method saves a new book
+    @PostMapping("/api/books")
+    @ResponseBody
+    public ResponseEntity<Book> createBook(@RequestBody Book newBook) {
+        try {
+        	// add and save new book 
+            Book savedBook = bookRepository.save(newBook);
+            return ResponseEntity.status(HttpStatus.CREATED).body(savedBook);
+        } catch (Exception e) {
+            // Handle exceptions and return an appropriate response
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
     
 }
